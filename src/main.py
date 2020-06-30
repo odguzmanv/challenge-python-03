e6f44b11ab3e5665f5d9b5c56e8d4498d01a2a88
Oscar Guzmán <odguzmanv@protonmail.com>
2020-06-30 18:03:34 -0500

    Desafio completado: christianvanderhenst

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 # Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
-
+    with open('./encoded.txt', 'r', encoding = 'utf-8') as f:
+        code = f.read()
+    decode=''.join(re.findall('[a-z]', code))
+    print(decode)
 
 if __name__ == '__main__':
     run()
